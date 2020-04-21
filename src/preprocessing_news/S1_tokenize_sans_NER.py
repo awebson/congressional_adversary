3f076b9d4d70c2e343f58ddda24a6d6cf1571a14
Albert Webson <awebson@cs.brown.edu>
2020-04-21 10:02:06 -0400

    tokenize with underscored NER

--- src/preprocessing_news/S1_tokenize_sans_NER.py
+++ src/preprocessing_news/S1_tokenize_sans_NER.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 import pickle
 from pathlib import Path
 from dataclasses import dataclass, field
-from typing import Tuple, List, Iterable, Optional
+from typing import List, Dict, Iterable, Optional
 
 import stanza
 from tqdm import tqdm
@@ -14,8 +14,6 @@ from tqdm import tqdm
 @dataclass
 class Sentence():
     tokens: List[List[str]]
-    underscored_tokens: List[List[str]]
-    entities: List[Tuple[str, str]]  # ent.type, ent.text
     normalized_tokens: List[List[str]] = field(default_factory=list)
     subsampled_tokens: List[List[str]] = field(default_factory=list)
     numerical_tokens: List[List[int]] = field(default_factory=list)
@@ -31,44 +29,7 @@ class LabeledDoc():
     text: str
     date: Optional[str] = None
     sentences: Optional[List[Sentence]] = None
-
-
-EXCLUDE = {'DATE', 'ORDINAL', 'CARDINAL', 'PERCENT', 'MONEY', 'TIME', 'QUANTITY'}
-
-def underscore_NER(tokens):
-    new_seq = []
-    for index, token in enumerate(tokens):
-        if token.ner == 'O':  # No NER
-            new_seq.append(token.text)
-            continue
-        prefix, ent_type = token.ner.split('-')
-        if ent_type in EXCLUDE:
-            new_seq.append('<NUM>')
-        elif prefix == 'S':  # single-token length
-            new_seq.append(token.text)
-
-        elif prefix == 'B':  # multi-token phrase
-            joint_token = [token.text, ]
-            for token_ahead in tokens[index + 1:]:
-                if token_ahead.ner == 'O':
-                    break
-                prefix_ahead, _ = token_ahead.ner.split('-')
-                if prefix_ahead == 'I':
-                    joint_token.append(token_ahead.text)
-                elif prefix_ahead == 'E':
-                    joint_token.append(token_ahead.text)
-                    break
-                elif prefix_ahead == 'B' or prefix_ahead == 'S':
-                    break
-                else:
-                    print(token, token.ner)
-                    print(token_ahead, token.ner)
-                    print([(t.text, t.ner) for t in tokens])
-                    raise RuntimeError('Malformed NER IOB tag')
-            new_seq.append('_'.join(joint_token))
-        else:
-            continue
-    return new_seq
+#     entities: Optional[List[Span]] = None
 
 
 def partition(corpus: List, num_chunks: int) -> Iterable[List]:
@@ -118,39 +79,21 @@ def main() -> None:
             print('Missing:', article.attrib)
     print(f'Number of nonempty articles = {len(data):,}')
 
-    stanza_processor = stanza.Pipeline(
-        lang='en', processors='tokenize,ner',
-        tokenize_batch_size=32, ner_batch_size=32)
-    # stanza_processor = stanza.Pipeline(
-    #   lang='en', processors={'tokenize': 'spacy'})
 
-
-    # NOTE further dividing data for distributing among GPUs
-    # data = data[:len(data) // 2]  # first half
-    # data = data[len(data) // 2:]  # second half
+    processor = stanza.Pipeline(
+        lang='en', processors='tokenize', tokenize_batch_size=20_000)
+    # processor = stanza.Pipeline(lang='en', processors={'tokenize': 'spacy'})
 
     out_dir = Path('../../data/interim/news/')
     Path.mkdir(out_dir, parents=True, exist_ok=True)
 
-    num_chunks = 100
     for part_index, some_docs in tqdm(
-            enumerate(partition(data, num_chunks)),
-            total=num_chunks,
-            desc='Total'):
-
-        # for doc in corpus:
-        #     doc.compressed = [
-        #         Sentence(underscore_NER(stanza_sent.tokens))
-        #         for stanza_sent in doc.sentences]
+            enumerate(partition(data, 60)), total=60, desc='Total'):
         for doc in tqdm(some_docs, desc='Chunk'):
-            processed = stanza_processor(doc.text)
-            doc.sentences = [  # throw away extra info to conserve disk space
-                Sentence(
-                    tokens=[token.text for token in stanza_sent.tokens],
-                    underscored_tokens=underscore_NER(stanza_sent.tokens),
-                    entities=[(ent.type, ent.text) for ent in stanza_sent.ents])
-                for stanza_sent in processed.sentences
-            ]
+            processed = processor(doc.text)
+            doc.sentences = [
+                Sentence([token.text for token in stanza_sent.tokens])
+                for stanza_sent in processed.sentences]
         with open(out_dir / f'tokenized_{part_index}.pickle', 'wb') as file:
             pickle.dump(some_docs, file, protocol=-1)
 
--- src/preprocessing_news/S3_export.py
+++ src/preprocessing_news/S3_export.py
@@ -3,13 +3,11 @@ import math
 import random
 import pickle
 from pathlib import Path
-from dataclasses import dataclass
-from typing import Tuple, NamedTuple, List, Dict, Iterable, Counter, Optional
+from typing import Tuple, List, Dict, Counter, Optional
 
-import stanza
 from tqdm import tqdm
 
-from S1_tokenize import Sentence, LabeledDoc
+from preprocessing_news.S1_tokenize import Sentence, LabeledDoc
 
 random.seed(42)
 
@@ -79,6 +77,8 @@ def main(
         out_dir: Path,
         num_corpus_parts: int,
         min_frequency: int,
+        min_sent_len: int,
+        max_sent_len: int,
         subsample_heuristic: Optional[str],
         subsample_threshold: float,
         conserve_RAM: bool = True  # turn off to inspect intermediate results
@@ -91,12 +91,14 @@ def main(
     Path.mkdir(out_dir, parents=True, exist_ok=True)
     preview = open(out_dir / f'preview.txt', 'w')
     print(f'Min word frequency = {min_frequency}', file=preview)
+    print(f'Min sentence length = {min_sent_len}', file=preview)
+    print(f'Max sentence length = {max_sent_len}', file=preview)
     print(f'SGNS subsample heuristic= {subsample_heuristic}', file=preview)
     print(f'SGNS subsample threshold = {subsample_threshold}', file=preview)
 
     # Lowercase, discard punctuations, replace numbers
     number = re.compile(r'\d')
-    nonalphanumeric = re.compile(r'\W')
+    nonalphanumeric = re.compile(r'\W')  # TODO NOTE exclude underscore
     norm_freq: Counter[str] = Counter()
     for doc in tqdm(corpus, desc='Normalize tokens'):
         for sent in doc.sentences:
@@ -126,56 +128,88 @@ def main(
     norm_freq = new_freq
     print(f'Filtered vocabulary size = {len(norm_freq):,}', file=preview)
 
-    # Subsampling
+    # Subsampling & filter by mix/max sentence length
     keep_prob = subsampling(norm_freq, subsample_heuristic, subsample_threshold)
     final_freq: Counter[str] = Counter()
-    for doc in tqdm(corpus, desc='Subsampling frequent words'):
+    for doc in tqdm(corpus, desc='Subsample frequent words'):
         for sent in doc.sentences:
             for token in sent.normalized_tokens:
                 if token not in norm_freq:
                     token = '<UNK>'
                 if random.random() < keep_prob[token]:
                     sent.subsampled_tokens.append(token)
-            final_freq.update(sent.subsampled_tokens)
+
+            if min_sent_len <= len(sent.subsampled_tokens) <= max_sent_len:
+                final_freq.update(sent.subsampled_tokens)
+            else:
+                sent.subsampled_tokens = None
             if conserve_RAM:
                 del sent.normalized_tokens
-    print(f'Final vocabulary size = {len(final_freq)}', file=preview)
+        doc.sentences = [
+            sent for sent in doc.sentences
+            if sent.subsampled_tokens is not None]
+    corpus = [doc for doc in corpus if len(doc.sentences) > 0]
+
+    print(f'Final vocabulary size = {len(final_freq):,}', file=preview)
     print(f'Subsampled number of words = '
           f'{sum(freq for freq in final_freq.values()):,}',
           file=preview)
 
+    # Numericalize corpus by word_ids
     word_to_id, id_to_word = build_vocabulary(final_freq)
-    for doc in tqdm(corpus, desc='Converting to word ids'):
+    for doc in tqdm(corpus, desc='Converting to word_ids'):
         for sent in doc.sentences:
             sent.numerical_tokens = [
                 word_to_id[token] for token in sent.subsampled_tokens]
             if conserve_RAM:
                 del sent.subsampled_tokens
 
-    with open(out_dir / 'train.pickle', 'wb') as out_file:
-        pickle.dump(corpus, out_file, protocol=-1)
+    # Helper for negative sampling
+    cumulative_freq = sum(freq ** 0.75 for freq in final_freq.values())
+    negative_sampling_probs: Dict[int, float] = {
+        word_to_id[word]: (freq ** 0.75) / cumulative_freq
+        for word, freq in final_freq.items()}
+    negative_sampling_probs: List[float] = [
+        # negative_sampling_probs[word_id]  # strict
+        negative_sampling_probs.get(word_id, 0)  # prob = 0 if missing vocab
+        for word_id in range(len(word_to_id))]
 
-    docs = random.sample(corpus, 100)
+    random.shuffle(corpus)
+    cucumbers = {
+        'word_to_id': word_to_id,
+        'id_to_word': id_to_word,
+        'negative_sampling_probs': negative_sampling_probs,
+        'documents': corpus}
+    print(f'Writing to {out_dir}')
+    with open(out_dir / 'train.pickle', 'wb') as out_file:
+        pickle.dump(cucumbers, out_file, protocol=-1)
+
+    # Print out vocabulary & some random sentences for sanity check
+    docs = random.sample(corpus, 50)
     preview.write('\n')
     for doc in docs:
+        sent = doc.sentences[0]
         if not conserve_RAM:
-            sent = doc.sentences[0]
             print(sent.tokens, file=preview)
             print(sent.normalized_tokens, file=preview)
             print(sent.subsampled_tokens, end='\n\n', file=preview)
         else:
-            for sent in doc.sentences:
-                print(sent.numerical_tokens, file=preview)
+            print(sent.numerical_tokens, file=preview)
             # print(vars(doc), end='\n\n', file=preview)
+    preview.write('\n')
+    for key, val in final_freq.most_common():
+        print(f'{val:,}:\t {key}', file=preview)
     preview.close()
 
 
 if __name__ == '__main__':
     main(
-        in_dir=Path('../../data/interim/news/toys'),
+        in_dir=Path('../../data/interim/news'),
         out_dir=Path('../../data/processed/news'),
         min_frequency=10,
-        num_corpus_parts=3,
+        min_sent_len=5,
+        max_sent_len=20,
+        num_corpus_parts=5,
         subsample_heuristic='paper',
         subsample_threshold=1e-3,
         conserve_RAM=True)
