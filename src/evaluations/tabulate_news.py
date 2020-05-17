d5d000c32342804f9b650953c249bea9fcd4eead
Albert Webson <awebson@cs.brown.edu>
2020-05-17 09:36:25 -0400

    fix diagonal sums

--- src/evaluations/tabulate_news.py
+++ src/evaluations/tabulate_news.py
@@ -1,24 +1,47 @@
 import csv
 from pathlib import Path
-from dataclasses import dataclass
-from typing import Set, Tuple, Union, List, Dict, Iterable
+from typing import Set, Tuple, List, Dict
 
 import numpy as np
 import torch
-from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
 from decomposer import Decomposer, DecomposerConfig
 from recomposer import Recomposer, RecomposerConfig
 
 
+def evaluate(word_ids, suffix, D_model, C_model) -> Dict[str, float]:
+    row = {}
+    DS_Hdeno, _, DS_Hcono = D_model.homogeneity(word_ids)
+    row['DS Hdeno'] = DS_Hdeno
+    row['DS Hcono'] = DS_Hcono
+    row['IntraDS Hd - Hc'] = DS_Hdeno - DS_Hcono
+
+    CS_Hdeno, _, CS_Hcono = C_model.homogeneity(word_ids)
+    row['CS Hdeno'] = CS_Hdeno
+    row['CS Hcono'] = CS_Hcono
+    row['IntraCS Hc - Hd'] = CS_Hcono - CS_Hdeno
+
+    row['Inter DS Hd - CS Hd'] = DS_Hdeno - CS_Hdeno
+    row['Inter CS Hc - DS Hc'] = CS_Hcono - DS_Hcono
+
+    row['main diagnoal trace'] = (DS_Hdeno + CS_Hcono) / 2  # max all preservation
+    row['nondiagnoal entries negative sum'] = (-DS_Hcono - CS_Hdeno) / 2  # min all discarded
+    row['flattened weighted sum'] = row['main diagnoal trace'] + row['nondiagnoal entries negative sum']
+
+    row['mean IntraS quality'] = (row['IntraDS Hd - Hc'] + row['IntraCS Hc - Hd']) / 2
+    row['mean InterS quality'] = (row['Inter DS Hd - CS Hd'] + row['Inter CS Hc - DS Hc']) / 2
+    return {key + f' ({suffix})': val for key, val in row.items()}
+
+
 def main() -> None:
     in_dir = Path('../../results/search')
-    patterns = ['*/epoch*.pt']  #,'duplicate/*/epoch*.pt']
+    patterns = ['*/epoch*.pt']  # 'duplicate/*/epoch*.pt']
     out_path = in_dir / 'diagonal.tsv'
     random_path = Path('../../data/ellie/rand_sample.hp.txt')
     test_path = Path('../../data/ellie/partisan_sample.hp.txt')
     device = torch.device('cuda:0')
+    debug = False
 
     # tensorboard = SummaryWriter(log_dir=log_dir)
     checkpoints: List[Path] = []
@@ -27,8 +50,8 @@ def main() -> None:
     if len(checkpoints) == 0:
         raise FileNotFoundError(f'No model with path pattern found at {in_dir}?')
 
-    with open(random_path) as file:
-        random_words = [word.strip() for word in file]
+    # with open(random_path) as file:
+    #     random_words = [word.strip() for word in file]
     with open(test_path) as file:
         test_words = [word.strip() for word in file]
 
@@ -60,8 +83,9 @@ def main() -> None:
         else:
             wid = model.word_to_id
             # random = torch.tensor([wid[word] for word in random_words], device=device)
-            # test = torch.tensor([wid[word] for word in test_words], device=device)
+            test_ids = torch.tensor([wid[word] for word in test_words], device=device)
 
+            # For OOV
             # def word_id_tensor(words: List[str]) -> torch.Tensor:
             #     wids = []
             #     skipped = []
@@ -74,97 +98,45 @@ def main() -> None:
             #     if skipped:
             #         print(f'{len(skipped)} OOV: {skipped}')
             #     return torch.tensor(wids, device=device)
-
             # random = word_id_tensor(random)
             # test = word_id_tensor(test)
 
-
             # Initailize denotation grounding
-            # def pretrained_neighbors(
-            #         query_ids: torch.Tensor,
-            #         top_k: int = 10
-            #         ) -> Dict[int, Set[int]]:
-            #     import editdistance
-            #     import torch.nn.functional as F
+            def pretrained_neighbors(
+                    query_ids: torch.Tensor,
+                    top_k: int = 10
+                    ) -> Dict[int, Set[int]]:
+                import editdistance
+                import torch.nn.functional as F
 
-            #     deno_grounding: Dict[int, Set[int]] = {}
-            #     for qid in query_ids:
-            #         qv = D_model.pretrained_embed(qid)
-            #         qid = qid.item()
-            #         qw = model.id_to_word[qid]
-            #         cos_sim = F.cosine_similarity(qv.unsqueeze(0), D_model.pretrained_embed.weight)
-            #         cos_sim, neighbor_ids = cos_sim.topk(k=top_k + 5, dim=-1)
-            #         neighbor_ids = [
-            #             nid for nid in neighbor_ids.tolist()
-            #             if editdistance.eval(qw, model.id_to_word[nid]) > 3]
-            #         deno_grounding[qid] = set(neighbor_ids[:top_k])
-            #     return deno_grounding
+                deno_grounding: Dict[int, Set[int]] = {}
+                for qid in query_ids:
+                    qv = D_model.pretrained_embed(qid)
+                    qid = qid.item()
+                    qw = model.id_to_word[qid]
+                    cos_sim = F.cosine_similarity(qv.unsqueeze(0), D_model.pretrained_embed.weight)
+                    cos_sim, neighbor_ids = cos_sim.topk(k=top_k + 5, dim=-1)
+                    neighbor_ids = [
+                        nid for nid in neighbor_ids.tolist()
+                        if editdistance.eval(qw, model.id_to_word[nid]) > 3]
+                    deno_grounding[qid] = set(neighbor_ids[:top_k])
+                return deno_grounding
 
-            # # rand_deno = pretrained_neighbors(random)
-            # test_deno = pretrained_neighbors(test)
+            # rand_deno = pretrained_neighbors(random)
+            test_deno = pretrained_neighbors(test_ids)
 
+        dev_ids = torch.cat([D_model.liberal_ids, D_model.neutral_ids, D_model.conservative_ids])
 
-        dev = torch.cat([D_model.liberal_ids, D_model.neutral_ids, D_model.conservative_ids])
-
-        DS_Hdeno, _, DS_Hcono = D_model.homogeneity(dev)
-        row['DS Hdeno'] = DS_Hdeno
-        row['DS Hcono'] = DS_Hcono
-        row['IntraDS Hd - Hc'] = DS_Hdeno - DS_Hcono
-
-        CS_Hdeno, _, CS_Hcono = C_model.homogeneity(dev)
-        row['CS Hdeno'] = CS_Hdeno
-        row['CS Hcono'] = CS_Hcono
-        row['IntraCS Hc - Hd'] = CS_Hcono - CS_Hdeno
-
-        row['Inter DS Hd - CS Hd'] = DS_Hdeno - CS_Hdeno
-        row['Inter CS Hc - DS Hc'] = CS_Hcono - DS_Hcono
-
-        row['main diagnoal trace'] = (DS_Hdeno + CS_Hcono) / 2  # max all preservation
-        row['negative antidiagnoal trace'] = (-DS_Hcono - CS_Hcono) / 2  # min all discarded
-        row['weighted trace'] = (row['main diagnoal trace'] + row['negative antidiagnoal trace']) / 2
-
-        row['mean IntraS quality'] = (row['IntraDS Hd - Hc'] + row['IntraCS Hc - Hd']) / 2
-        row['mean InterS quality'] = (row['Inter DS Hd - CS Hd'] + row['Inter CS Hc - DS Hc']) / 2
-        table.append(row)
-
+        D_model.deno_grounding.update(test_deno)
+        C_model.deno_grounding.update(test_deno)
         # D_model.deno_grounding.update(rand_deno)
-        # D_model.deno_grounding.update(test_deno)
         # C_model.deno_grounding.update(rand_deno)
-        # C_model.deno_grounding.update(test_deno)
 
-
-        # # DH_rand, CKL_rand, CH_rand = D_model.homogeneity(random)
-        # DH_dev, CKL_dev, CH_dev = D_model.homogeneity(dev)
-        # # DH_test, CKL_test, CH_test = D_model.homogeneity(test)
-        # row['DVec Dhomo'] = DH_dev
-        # row['DVec Chomo'] = CH_dev
-
-        # # D_rand_diff = DH_rand - CH_rand
-        # D_dev_diff = DH_dev - CH_dev
-        # # D_test_diff = DH_test - CH_test
-        # # row['DVec rand homo diff'] = D_rand_diff
-
-
-        # row['DVec/dev homo diff'] = D_dev_diff
-        # # row['DVec test homo diff'] = D_test_diff
-
-        # # DH_rand, CKL_rand, CH_rand = C_model.homogeneity(random)
-        # DH_dev, CKL_dev, CH_dev = C_model.homogeneity(dev)
-        # # DH_test, CKL_test, CH_test = C_model.homogeneity(test)
-        # row['CVec Dhomo'] = DH_dev
-        # row['CVec Chomo'] = CH_dev
-
-        # # C_rand_diff = CH_rand - DH_rand
-        # C_dev_diff = CH_dev - DH_dev
-        # # C_test_diff = CH_test - DH_test
-        # # row['CVec rand homo diff'] = C_rand_diff
-        # row['CVec dev homo diff'] = C_dev_diff
-        # # row['CVec test homo diff'] = C_test_diff
-
-        # # row['mean rand homo diff'] = np.mean([D_rand_diff, C_rand_diff])
-        # row['mean dev homo diff'] = np.mean([D_dev_diff, C_dev_diff])
-        # # row['mean test homo diff'] = np.mean([D_test_diff, C_test_diff])
-        # table.append(row)
+        row.update(evaluate(dev_ids, 'dev', D_model, C_model))
+        row.update(evaluate(test_ids, 'test', D_model, C_model))
+        table.append(row)
+        if debug is True:
+            break
 
     row = {
         'path': 'pretrained',
@@ -172,30 +144,12 @@ def main() -> None:
     }
     D_model.embedding = D_model.pretrained_embed
     C_model.embedding = C_model.pretrained_embed
-    DS_Hdeno, _, DS_Hcono = D_model.homogeneity(dev)
-    row['DS Hdeno'] = DS_Hdeno
-    row['DS Hcono'] = DS_Hcono
-    row['IntraDS Hd - Hc'] = DS_Hdeno - DS_Hcono
-
-    CS_Hdeno, _, CS_Hcono = C_model.homogeneity(dev)
-    row['CS Hdeno'] = CS_Hdeno
-    row['CS Hcono'] = CS_Hcono
-    row['IntraCS Hc - Hd'] = CS_Hdeno - CS_Hcono
-
-    row['Inter DS Hd - CS Hd'] = DS_Hdeno - CS_Hdeno
-    row['Inter CS Hc - DS Hc'] = CS_Hcono - DS_Hcono
-
-    row['main diagnoal trace'] = (DS_Hdeno + CS_Hcono) / 2  # max all preservation
-    row['antidiagnoal trace'] = (DS_Hcono + CS_Hcono) / 2  # min all discarded
-    row['mean trace'] = (row['main diagnoal trace'] + row['antidiagnoal trace']) / 2
-
-    row['mean IntraS quality'] = (row['IntraDS Hd - Hc'] + row['IntraCS Hc - Hd']) / 2
-    row['mean InterS quality'] = (row['Inter DS Hd - CS Hd'] + row['Inter CS Hc - DS Hc']) / 2
+    row.update(evaluate(dev_ids, 'dev', D_model, C_model))
+    row.update(evaluate(test_ids, 'test', D_model, C_model))
     table.append(row)
 
-
     with open(out_path, 'w') as file:
-        writer = csv.DictWriter(file, fieldnames=table[1].keys(), dialect=csv.excel_tab)
+        writer = csv.DictWriter(file, fieldnames=table[0].keys(), dialect=csv.excel_tab)
         writer.writeheader()
         writer.writerows(table)
 
